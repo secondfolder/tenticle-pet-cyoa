041ac7439560cbdb0ad0eb09bb44fc043572a583
Glitch (tentacle-pet) <none>
2021-06-12 13:34:21 +0000

    👧👴 Checkpoint ./package.json:578098/510 ./stay-up-hack.js:578098/216

---
+++ stay-up-hack.js
@@ -0,0 +1,10 @@
+const execSync = require("child_process").execSync;
+execSync("npx http-server ./ -p 3000 -c-1");
+
+process.on("beforeExit", code => {
+  execSync("refresh");
+});
+
+process.on("exit", code => {
+  execSync("refresh");
+});
