d9b15c38e6569f45f0cb7117ea2cab444da5cf32
Glitch (tentacle-pet) <none>
2021-12-14 21:57:02 +0000

    🌲🎠 Checkpoint ./package.json:578098/1340 ./stay-up-hack.js:578098/18

--- stay-up-hack.js
+++ stay-up-hack.js
@@ -1,10 +1,10 @@
 const execSync = require("child_process").execSync;
 execSync("npx http-server build -p 3000 -c60 --gzip --proxy http://localhost:${PORT}?");
 
-// process.on("beforeExit", code => {
-//   execSync("refresh");
-// });
+process.on("beforeExit", code => {
+  execSync("refresh");
+});
 
-// process.on("exit", code => {
-//   execSync("refresh");
-// });
+process.on("exit", code => {
+  execSync("refresh");
+});
